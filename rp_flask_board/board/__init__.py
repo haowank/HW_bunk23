b1d2b39d77a539d64e3781cb4bebf134a3e84fc3
LUONG HAO WEN 2000897E <2000897E@student.tp.edu.sg>
2024-04-02 17:20:48 +0800

    hw _day 2 -add db

--- rp_flask_board/board/__init__.py
+++ rp_flask_board/board/__init__.py
@@ -1,9 +1,25 @@
+import os
+from dotenv import load_dotenv
+
+
+from board import pages,posts,database
 from flask import Flask
-from board import pages
+from board import pages, posts
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_prefixed_env()
+
+    database.init_app(app)
+    
     app.register_blueprint(pages.bp)
+    app.register_blueprint(posts.bp)
+    print(f"Current env:{os.getenv('ENVIROMENT')}")
+    print(f"usng databse:{app.config.get('DATABASE')}")
+          
+    
 
     return app
 
